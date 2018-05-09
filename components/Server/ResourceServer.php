be6278b5051cc6f4779b86c706c52a8a6c7f8d2b
chervand <chervand@gmail.com>
2018-05-09 15:52:14 +0200

    Update `ResourceServer` namespace

--- components/Server/ResourceServer.php
+++ components/Server/ResourceServer.php
@@ -1,5 +1,6 @@
 <?php
-namespace chervand\yii2\oauth2\server\components;
+
+namespace chervand\yii2\oauth2\server\components\Server;
 
 class ResourceServer extends \League\OAuth2\Server\ResourceServer
 {
