1c9f7f53e47167bff5e3a0e6a8392db2f163ae6e
chervand <chervand@gmail.com>
2018-05-10 14:07:34 +0200

    Add parsed token to authorization event

--- components/Events/AuthorizationEvent.php
+++ components/Events/AuthorizationEvent.php
@@ -2,19 +2,48 @@
 
 namespace chervand\yii2\oauth2\server\components\Events;
 
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Token;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use yii\base\Event;
+use yii\helpers\Json;
 
 class AuthorizationEvent extends Event
 {
     const USER_AUTHENTICATION_SUCCEED = 'user.authentication.succeed';
 
     /**
-     * @var \Psr\Http\Message\ServerRequestInterface
+     * @var ServerRequestInterface
      */
     public $request;
 
     /**
-     * @var \Psr\Http\Message\ResponseInterface
+     * @var ResponseInterface
      */
     public $response;
+
+    /**
+     * @var Token
+     */
+    private $_token;
+
+
+    /**
+     * @return Token
+     */
+    public function getToken()
+    {
+        if (
+            !$this->_token instanceof Token
+            && $this->response instanceof ResponseInterface
+        ) {
+            $response = Json::decode($this->response->getBody()->__toString());
+            if (array_key_exists('access_token', $response)) {
+                $this->_token = (new Parser())->parse($response['access_token']);
+            }
+        }
+
+        return $this->_token;
+    }
 }
